1e3a4e6082605b48bd86485d6a9136d8267ad41f
mkarampatsis <marka@central.ntua.gr>
2025-03-27 18:53:59 +0200

    post

--- myapp2/app.js
+++ myapp2/app.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.use('/', express.static('files'));
 
 app.get('/', (req, res)=>{
   res.send("This is the home page");
@@ -15,15 +18,25 @@ app.post('/user', (req, res) => {
 
   console.log(data);
   res.send(`User data ${username}, ${email}`);
-})
+});
 
-app.post('userForm', (req, res)=>{
+app.post('/userForm', (req, res)=>{
   let data = req.body
 
   console.log("Data", data);
   res.send("UserForm Page");
-})
+});
+
+app.use('/user1', (req, res)=>{
+  console.log("User 1")
+  res.send("User 1 Page");
+});
+
+app.use('/user2', (req, res)=>{
+  console.log("User 2")
+  res.send("User 2 Page");
+});
 
 app.listen(port, ()=>{
   console.log("Server is up");
-})
+});
