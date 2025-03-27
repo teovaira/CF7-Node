d5e48afbbc8ebab64c179dd1ed247939be772dcf
mkarampatsis <marka@central.ntua.gr>
2025-03-27 20:40:19 +0200

    post

--- myapp4/dist/app.js
+++ myapp4/dist/app.js
@@ -4,10 +4,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const utils_1 = require("./utils");
 const app = (0, express_1.default)();
 const port = 3000;
 app.get('/', (req, res) => {
     res.send("Hello typescript");
+});
+app.get('/greetings', (req, res) => {
+    const message = (0, utils_1.greet)("World 2");
+    res.send(message);
 });
 app.listen(port, () => {
     console.log("Server is up");
--- myapp4/src/app.ts
+++ myapp4/src/app.ts
@@ -1,10 +1,16 @@
 import express from 'express';
+import { greet } from './utils';
 
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send("Hello typescript");
+});
+
+app.get('/greetings', (req, res) => {
+  const message = greet("World 2");
+  res.send(message);
 });
 
 app.listen(port, () => {
