37ad154e2ca795dd1a3c8b511f57c7750f0d53e4
mkarampatsis <marka@central.ntua.gr>
2025-03-28 18:16:30 +0200

    middleware

--- myapp2/app.js
+++ myapp2/app.js
@@ -7,11 +7,20 @@ app.use(express.urlencoded({extended:true}))
 
 app.use('/', express.static('files'));
 
+const logger = (req, res, next) => {
+  let url = req.url;
+  console.log("Logger", req.body);
+  let time = new Date();
+  console.log('Received requests for ' + url + ' at ' + time);
+
+  next()
+}
+
 app.get('/', (req, res)=>{
   res.send("This is the home page");
 });
 
-app.post('/user', (req, res) => {
+app.post('/user', logger, (req, res) => {
   let data = req.body;
   let username = req.body.username;
   let email = req.body.email;
