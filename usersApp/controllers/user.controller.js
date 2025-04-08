81d0e34684ec587084b2148c8695c4d44333007b
mkarampatsis <marka@central.ntua.gr>
2025-04-08 18:26:49 +0300

    winston

--- usersApp/controllers/user.controller.js
+++ usersApp/controllers/user.controller.js
@@ -11,11 +11,11 @@ exports.findAll = async(req, res) => {
     // const result = await User.find();
     const result = await userService.findAll();
     res.status(200).json({status: true, data: result});
-    logger.info("INFO, Success in reading all users");
-    logger.warn("INFO, Success in reading all users");
+    logger.info("Success in reading all users");
+    logger.warn("Success in reading all users");
   } catch (err) {
     console.log("Problem in reading users", err);
-    logger.error("ERROR, Problem in reading all users", err);
+    logger.error("Problem in reading all users", err);
     res.status(400).json({status:false, data: err});
   }
 }
--- usersApp/logger/logger.js
+++ usersApp/logger/logger.js
@@ -1,12 +1,33 @@
-const winston = require('winston');
 
-const logger = winston.createLogger(
-  {
-    format: winston.format.json(),
-    transports: [
-      new winston.transports.Console()
-    ]
-  }
-)
+// First Example
+
+// const winston = require('winston');
+// const logger = winston.createLogger(
+//   {
+//     format: winston.format.json(),
+//     transports: [
+//       new winston.transports.Console()
+//     ]
+//   }
+// )
+
+// Second Example
+const { format, createLogger, transports } = require('winston')
+const { combine, timestamp, label, printf } = format
+const CATEGORY  = "Products app logs"
+
+const customFormat = printf(({message, label, timestamp})=>{
+    return `${timestamp} [${label}: ${message}]`;
+})
+
+const logger = createLogger({
+  // level: "warn",
+  format: combine(
+    label({label: CATEGORY}),
+    timestamp(),
+    customFormat
+  ),
+  transports: [new transports.Console()]
+})
 
 module.exports = logger;
