cb0e9fa61afe0e632603451c9e6588e4dcfc84e6
mkarampatsis <marka@central.ntua.gr>
2025-04-08 18:54:27 +0300

    winston

--- usersApp/logger/logger.js
+++ usersApp/logger/logger.js
@@ -12,22 +12,52 @@
 // )
 
 // Second Example
-const { format, createLogger, transports } = require('winston')
-const { combine, timestamp, label, printf } = format
-const CATEGORY  = "Products app logs"
+// const { format, createLogger, transports } = require('winston')
+// const { combine, timestamp, label, printf } = format
+// const CATEGORY  = "Products app logs"
 
-const customFormat = printf(({message, label, timestamp})=>{
-    return `${timestamp} [${label}: ${message}]`;
-})
+// const customFormat = printf(({level, message, label, timestamp})=>{
+//     return `${timestamp} [${label}: ${level}, ${message}]`;
+// })
+
+// const logger = createLogger({
+//   // level: "warn",
+//   format: combine(
+//     label({label: CATEGORY}),
+//     timestamp(),
+//     customFormat
+//   ),
+//   transports: [new transports.Console()]
+// })
+
+// Third Example
+require('winston-daily-rotate-file');
+const { format, createLogger, transports } = require('winston');
+const { combine, timestamp, label, printf, prettyPrint } = format;
+const CATEGORY  = "Products app logs";
+
+const fileRotateTransport = new transports.DailyRotateFile({
+  filename: "./logs/rotate-%DATE%.log",
+  datePattern: "DD-MM-YYYY",
+  maxFiles: "7d"
+});
 
 const logger = createLogger({
-  // level: "warn",
   format: combine(
-    label({label: CATEGORY}),
-    timestamp(),
-    customFormat
+    label({label: "MY LABEL FOR PRODUCTS APP"}),
+    timestamp({format:"DD-MM-YYYY HH:mm:sss"}),
+    format.json()
+    // prettyPrint()
   ),
-  transports: [new transports.Console()]
+  transports: [
+    new transports.Console(),
+    fileRotateTransport,
+    new transports.File(
+      {
+        filename:"logs/example.log"
+      }
+    )
+  ]
 })
 
 module.exports = logger;
