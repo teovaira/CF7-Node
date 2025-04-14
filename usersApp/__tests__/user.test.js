e9c2d2f51e40478a8e0ab3fa80487b136eabf47e
mkarampatsis <marka@central.ntua.gr>
2025-04-14 21:06:15 +0300

    jest tests

--- usersApp/__tests__/user.test.js
+++ usersApp/__tests__/user.test.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const request = require("supertest");
 
 const authService = require('../services/auth.service');
+const userService = require('../services/user.services');
 
 const app = require('../app');
 // require('dotenv').config();
@@ -50,21 +51,25 @@ describe("Requests for /api/users/:username", () => {
 
   beforeAll(()=>{
     user = {
-      username: "admin",
-      email: "admin@aueb.gr",
+      username: "lakis",
+      email: "lalakis@aueb.gr",
       roles: ["EDITOR", "READER","ADMIN"]
     };
     token = authService.generateAccessToken(user);
   });
 
   it("Get returns specific user", async()=>{
+    
+    const result = await userService.findLastInsertedUser();
+    console.log("RESULT>>", result);
+    
     const res = await request(app)
-      .get('/api/users/user1')
+      .get('/api/users/'+result.username)
       .set('Authorization', `Bearer ${token}`); 
     
     expect(res.statusCode).toBe(200);
     expect(res.body.status).toBeTruthy();
-    expect(res.body.username).toBe('user1');
-    expect(res.body.email).toBe('user1@aueb.gr');
+    expect(res.body.data.username).toBe(result.username);
+    expect(res.body.data.email).toBe(result.email);
   })
 })
--- usersApp/services/user.services.js
+++ usersApp/services/user.services.js
@@ -10,4 +10,17 @@ function findOne(username) {
   return result;
 }
 
-module.exports = { findAll, findOne }
+async function findLastInsertedUser(){
+  console.log("Find last inserted user");
+
+  try {
+    const result = await User.find().sort({_id:-1}).limit(1);
+    console.log("Success in finding last inserted user", result[0]);
+    return result[0]
+  } catch (err){
+    console.log("Problem in finding last inserted user", err);
+    return false
+  }
+}
+
+module.exports = { findAll, findOne, findLastInsertedUser }
