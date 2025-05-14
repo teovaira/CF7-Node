8fcf9040648d87f83ec98a6daf9586e2792109b5
mkarampatsis <marka@central.ntua.gr>
2025-05-14 19:57:32 +0300

    Change post for create

--- usersApp/routes/user.routes.js
+++ usersApp/routes/user.routes.js
@@ -8,8 +8,8 @@ const verifyRoles = require('../middlewares/auth.middleware').verifyRoles;
 router.get('/',verifyToken, userController.findAll);
 router.get('/:username', verifyToken, userController.findOne);
 // router.post('/',userController.create);
-// router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create);
-router.post('/', userController.create);
+router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create);
+// router.post('/', userController.create);
 router.patch('/:username', verifyToken, verifyRoles("ADMIN"), userController.update);
 router.delete('/:username', verifyToken, verifyRoles("ADMIN"), userController.deleteByUsername);
 router.delete('/:username/email/:email', verifyToken, verifyRoles("ADMIN"), userController.deleteByEmail )
