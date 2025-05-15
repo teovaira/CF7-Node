3ce18ae1b402c0277d59440b8f1bf0c9be329563
Th. Vairaktaris <t.vairaktaris@gmail.com>
2025-05-15 18:31:22 +0300

    changes to cooperate with our front end

--- usersApp/controllers/auth.controller.js
+++ usersApp/controllers/auth.controller.js
@@ -35,7 +35,9 @@ exports.googleLogin = async(req, res) => {
     let user = await authService.googleAuth(code);
     if (user){
       console.log(">>>", user);
-      res.status(200).json({status: true, data: user});
+      // res.status(200).json({status: true, data: user});
+      const frontendRedirectUrl = `http://localhost:4200/login?token=${user}`
+      return res.redirect(frontendRedirectUrl)
     } else {
       res.status(400).json({status:false, data: "Problem in Google Login"});
     }
--- usersApp/routes/user.routes.js
+++ usersApp/routes/user.routes.js
@@ -9,8 +9,10 @@ router.get('/',verifyToken, userController.findAll);
 router.get('/:username', verifyToken, userController.findOne);
 // router.post('/',userController.create);
 router.post('/', verifyToken, verifyRoles("ADMIN"), userController.create);
+// router.post('/', userController.create);
 router.patch('/:username', verifyToken, verifyRoles("ADMIN"), userController.update);
 router.delete('/:username', verifyToken, verifyRoles("ADMIN"), userController.deleteByUsername);
 router.delete('/:username/email/:email', verifyToken, verifyRoles("ADMIN"), userController.deleteByEmail )
+router.get('/check_duplicate_email/:email', userController.checkDuplicateEmail);
 
 module.exports = router;
